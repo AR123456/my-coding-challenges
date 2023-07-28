b875b9ffdb485808fb08d783d07ea3e2fd7879f9
AR123456 <coloradoz99@gmail.com>
2023-07-28 07:20:37 -0600

    renaming and organizing

---
+++ 100 Cents/minCoins.js
@@ -0,0 +1,23 @@
+//  Find the least number of coins required that can make any change from 1 to 99 cents. The coins can only be pennies (1), nickels (5), dimes (10), and quarters (25), and you must be able to make every value from 1 to 99 (in 1-cent increments) using those coins.
+
+let coins = [1, 5, 10, 25];
+let n = coins.length;
+let arrayOfCoins = [];
+
+function minCoins(x) {
+  for (let i = n - 1; i >= 0; i--) {
+    while (x >= coins[i]) {
+      x -= coins[i];
+      arrayOfCoins.push(coins[i]);
+    }
+    console.log("array of coins", arrayOfCoins);
+
+    const count = arrayOfCoins.reduce((accumulator, value) => {
+      accumulator[value] = ++accumulator[value] || 1;
+
+      return accumulator;
+    }, {});
+    console.log("count", count);
+  }
+}
+minCoins(73);
