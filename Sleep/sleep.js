ee6dac675ff8279228719a9a194bbc7a8edd2100
AR123456 <coloradoz99@gmail.com>
2023-07-30 10:02:26 -0600

    solution one

--- Sleep/sleep.js
+++ Sleep/sleep.js
@@ -1,1 +1,16 @@
-// todays chanalge
+/**
+ *Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+ *
+ *Constraints:
+
+1 <= millis <= 1000
+ *
+ *Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now()
+ * @param {number} millis
+ */
+async function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+let t = Date.now();
+sleep(200).then(() => console.log(Date.now() - t));
