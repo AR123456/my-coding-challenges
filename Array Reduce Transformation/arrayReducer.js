474ec1710fef1e80bf1c81a1490555af4d14aa68
AR123456 <coloradoz99@gmail.com>
2023-08-05 07:04:13 -0600

    logging out some stuff

---
+++ Array Reduce Transformation/arrayReducer.js
@@ -0,0 +1,27 @@
+/* Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+If the length of the array is 0, it should return init.
+
+Please solve it without using the built-in Array.reduce method. */
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+
+nums = [1, 2, 3, 4];
+fn = function sum(accum, curr) {
+  return accum + curr;
+};
+init = 0;
+var reduce = function (nums, fn, init) {
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    console.log(nums, fn(nums, init), fn(nums[i], init));
+  }
+};
+reduce(nums, fn, init);
