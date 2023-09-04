f3ae23b8014c8a0d80a7ce14efd913510ed7cae1
AR123456 <coloradoz99@gmail.com>
2023-09-04 05:54:19 -0600

    not working try 1

---
+++ Valid anagram/ValidAnagram1.js
@@ -0,0 +1,38 @@
+const { element } = require("prop-types");
+
+/**
+ * iven two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+onstraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+(s = "anagram"), (t = "nagaram");
+var isAnagram = function (s, t) {
+  //  do string s and t contain the same characters?
+  for (s of s) {
+    console.log(s);
+  }
+};
+
+isAnagram(s, t);
+/* G
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+C */
