7a050200726beb713429c96224b67cccac4e700f
AR123456 <coloradoz99@gmail.com>
2023-09-13 07:18:42 -0600

    commented

---
+++ Same Tree/sameTreeCommented.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ Constraints:
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+/* This function recursively checks whether the nodes of the two trees are equal in value and structure. If both nodes are null, it returns true. If one node is null and the other is not, it returns false. If the values of the nodes are not equal, it returns false. Otherwise, it recursively checks the left and right subtrees of the nodes. */
+var isSameTree = function (p, q) {
+  if (p === null && q === null) {
+    return true;
+  } else if (p === null || q === null) {
+    return false;
+  } else if (p.val !== q.val) {
+    return false;
+  } else {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+};
+/* Example 1:
+
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false */
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+const p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+const q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+console.log(isSameTree(p, q)); // true
