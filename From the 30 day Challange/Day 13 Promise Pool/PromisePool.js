5e9431db2a460712980003b56e387932592f79cd
AR123456 <coloradoz99@gmail.com>
2023-09-29 05:04:38 -0600

    Promise Pool

---
+++ From the 30 day Challange/Day 13 Promise Pool/PromisePool.js
@@ -0,0 +1,8 @@
+// @param {Function []}functions
+// @param {number}n
+// @param {Function }
+
+var promisePool = async function (functions, n) {};
+
+// const sleep = (t) => NewPromise((res) => setTimeout(res, t));
+// promisePool([() => sleep(500), () => sleep(400)], 1).then(console.log); // after 900ms
