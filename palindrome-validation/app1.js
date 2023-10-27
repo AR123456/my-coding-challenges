8ef07aef3654898f679dee81eb7afc4bab398661
AR123456 <coloradoz99@gmail.com>
2023-10-27 07:12:32 -0600

    some psudo code

---
+++ palindrome-validation/app1.js
@@ -0,0 +1,10 @@
+// CHALLENGE 2: VALIDATE A PALINDROME
+//https://www.youtube.com/watch?v=M2bJBuaOeOQ
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) {
+  // push str into array
+  // use array reverse method to push to new array
+  // check if array one and two match check if
+}
