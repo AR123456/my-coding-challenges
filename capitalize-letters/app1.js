d3594e34231d5411c13ac7b14170f1af1f408814
AR123456 <coloradoz99@gmail.com>
2023-10-29 03:44:34 -0600

    sol one

--- capitalize-letters/app1.js
+++ capitalize-letters/app1.js
@@ -2,14 +2,7 @@
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-  let strArr = [];
-  strArr = str.split(" ");
-  console.log(strArr);
-  for (let i = 0; i < strArr.length; i++) {
-    if ([i].length === 0) {
-      i.toUpperCase();
-    }
-    console.log(strArr);
-  }
+
+  console.log(str.toUpperCase());
 }
 capitalizeLetters("i love javascript");
