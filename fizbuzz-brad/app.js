2bcfd1212bb966ee08976bdf9ad712df2787b529
AR123456 <coloradoz99@gmail.com>
2023-11-01 06:26:26 -0600

    logging the nums 1-100

--- fizbuzz-brad/app.js
+++ fizbuzz-brad/app.js
@@ -2,15 +2,9 @@
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 function fizzBuzz(nums) {
-  for (let i = 0; i < nums; i++) {
-    if (nums % 3 === 0) {
-      console.log("Fizz");
-    }
-    if (nums % 5 === 0) {
-      console.log("Buzz");
-    }
-    if (nums % 3 === 0 || nums % 5 === 0) console.log("Fizz Buzz");
+  for (let i = 1; i <= 100; i++) {
+    console.log(i);
   }
 }
 
-fizzBuzz(100);
+fizzBuzz();
