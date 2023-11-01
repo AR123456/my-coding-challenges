2c5d24114bfaafdb0a779c6c12e718e273f0f6cf
AR123456 <coloradoz99@gmail.com>
2023-11-01 06:32:38 -0600

    no fiz buzz

--- fizbuzz-brad/app.js
+++ fizbuzz-brad/app.js
@@ -7,11 +7,16 @@ function fizzBuzz(nums) {
     if (i % 3 === 0) {
       console.log("Fizz");
     } else {
-      console.log(i);
+      if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        if (i % 3 === 0 || i % 5 === 0) {
+          console.log("FizzBuzz");
+        } else {
+          console.log(i);
+        }
+      }
     }
-    // if (i % 5 === 0) {
-    //   console.log("Buzz");
-    // }
   }
 }
 
