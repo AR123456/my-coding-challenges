cc3dc6803a981c681d1cfcf209d242ddf15ac406
AR123456 <coloradoz99@gmail.com>
2023-11-03 05:52:32 -0600

    psudo code

--- array-chunking/app.js
+++ array-chunking/app.js
@@ -3,4 +3,13 @@
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) {}
+function chunkArray(arr, len) {
+  // arr input a long array
+  // len input the length of arrays
+  // the chunkArray is an array of smaller arrays
+  arr.map(function () {
+    newArr = arr.pop(len);
+  });
+  console.log(newArr);
+}
+chunkArray([1, 2, 3, 4, 5, 6, 7], 3);
