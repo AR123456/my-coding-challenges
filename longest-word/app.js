16f763d3a1bb1879b71f134ca4d42633f1de319a
AR123456 <coloradoz99@gmail.com>
2023-11-03 05:36:40 -0600

    final part of longest word problem

--- longest-word/app.js
+++ longest-word/app.js
@@ -8,16 +8,25 @@
 function longestWord(sen) {
   // strip away punctuation and spaces
   const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
-  console.log(wordArr);
+  // console.log(wordArr);
   // sort by length using sort method
   const sorted = wordArr.sort(function (a, b) {
     return b.length - a.length;
   });
-  console.log(sorted[0]);
+  // console.log(sorted[0]);
   // if multiple words are longest , put them in an array and compare using filter method
   const longestWordArr = sorted.filter(function (word) {
     return word.length === sorted[0].length;
   });
-  console.log(longestWordArr);
+
+  // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+  // check for more than one long return the word
+  if (longestWordArr.length === 1) {
+    console.log(longestWordArr[0]);
+    return longestWordArr[0];
+  } else {
+    console.log(longestWordArr);
+    return longestWordArr;
+  }
 }
-longestWord("Hello there, my name is Brad");
+longestWord("Hello there, my name is Anne");
