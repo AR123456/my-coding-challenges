1886a6f7580f4e6759ca7d32e10a47d606a48d5e
AR123456 <coloradoz99@gmail.com>
2023-11-07 10:02:38 -0700

    the correct problem and some psudocode

--- flatten-array/app.js
+++ flatten-array/app.js
@@ -1,6 +1,5 @@
 function flattenArray(arrays) {}
 
-// CHALLENGE 4: ANAGRAM
-// Return true if anagram and false if not
-// ex. 'elbow' === 'below'
-// ex. 'Dormitory' === 'dirty room##'
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
---
+++ flatten-array/app1.js
@@ -0,0 +1,9 @@
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+let sourceArr = [[1, 2], [3, 4], [5, 6], [7]];
+function flattenArray(arrays) {
+  // take the given array and use
+  console.log(sourceArr.join(","));
+}
+flattenArray(sourceArr);
