d74ea2ff675f1842ae34ee503350ae85e71f0c94
AR123456 <coloradoz99@gmail.com>
2023-11-08 06:39:49 -0700

    concat apply

--- flatten-array/appReduce.js
+++ flatten-array/appReduce.js
@@ -1,9 +1,15 @@
+function flattenArray(arrays) {}
+
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 let sourceArr = [[1, 2], [3, 4], [5, 6], [7]];
 function flattenArray(arrays) {
-  // concats apply method
-  //  property from concat() that does the heavy lifting, “apply” just executes the method
+  // use reduce  reduce accumulator,callbackFunction
+  // concat b onto a
+  return arrays.reduce(function (a, b) {
+    console.log(a.concat(b));
+    return a.concat(b);
+  });
 }
 flattenArray(sourceArr);
