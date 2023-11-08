3c81a76695f91ad6e78be5a52668a14835a0d451
AR123456 <coloradoz99@gmail.com>
2023-11-08 06:31:55 -0700

    not working reduce

--- flatten-array/app1.js
+++ flatten-array/app1.js
@@ -3,7 +3,15 @@
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 let sourceArr = [[1, 2], [3, 4], [5, 6], [7]];
 function flattenArray(arrays) {
-  // take the given array and use
-  console.log(sourceArr.join(","));
+  let newArr = [];
+  let acc;
+  // use reduce  reduce(accumulator,callbackFunction())
+
+  arrays.reduce(acc, function () {
+    for (let acc = 0; acc < sourceArr.length; acc++) {
+      sourceArr.concat(newArr);
+    }
+    console.log(newArr);
+  });
 }
 flattenArray(sourceArr);
