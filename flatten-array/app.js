9e57fc008aaf774340ffe979bd0ecbf38f8695c4
AR123456 <coloradoz99@gmail.com>
2023-11-08 06:54:02 -0700

    concat apply way

--- flatten-array/app.js
+++ flatten-array/app.js
@@ -1,5 +1,6 @@
-function flattenArray(arrays) {}
-
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+let sourceArr = [[1, 2], [3, 4], [5, 6], [7]];
+function flattenArray(arrays) {}
+flattenArray(sourceArr);
