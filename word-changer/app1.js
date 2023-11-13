5867650f9c826152b2d478c29e7c8e965b930ddf
AR123456 <coloradoz99@gmail.com>
2023-11-13 06:16:39 -0700

    psudo code

---
+++ word-changer/app1.js
@@ -0,0 +1,12 @@
+// problem solve // CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+
+function letterChanges(str) {
+  // create an array of lower and Upper  case letters
+  // turn the str into an array include the space as a space
+  // create a holer array for the outpust
+  // use for loop if the letter at the index is in the alpha bit array replace it with the letter a the next index, push that in to the holder array
+  // if it is a space just push the space
+}
