935928db69488ce5f1bc285d226e08f182ba5c3f
AR123456 <coloradoz99@gmail.com>
2023-11-14 06:10:53 -0700

    add nums

---
+++ add-all-numbers/app1.js
@@ -0,0 +1,11 @@
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
+// ex. addAll(2,5,6,7) === 20
+
+function addAll(nums) {
+  nums+=;
+  
+  
+  console.log(nums);
+}
+addAll(1, 2, 3);
