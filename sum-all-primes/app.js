50065e0b965f0e65f002f13c0d3b8fd2e126d30a
AR123456 <coloradoz99@gmail.com>
2023-11-15 05:53:39 -0700

    sum all primes problem

---
+++ sum-all-primes/app.js
@@ -0,0 +1,5 @@
+// CHALLENGE 2: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex. sumAllPrimes(10) == 17
+
+function sumAllPrimes() {}
