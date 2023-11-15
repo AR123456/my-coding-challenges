047d0f4cb63630a1cb4b459a4ccb29d2d9cc3b7a
AR123456 <coloradoz99@gmail.com>
2023-11-15 05:51:52 -0700

    all the ways to solve

--- add-all-numbers/app1.js
+++ add-all-numbers/app1.js
@@ -2,12 +2,11 @@
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
 
-function addAll(nums) {
-  let sum = 0;
-  while (nums) {
-    sum += nums % 10;
-    nums = Math.floor(nums / 10);
-  }
-  console.log(sum);
+// function addAll(...rest) {
+function addAll(...numbers) {
+  total = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+  console.log(total);
 }
-addAll(1, 2, 3);
+addAll(2, 5, 6, 7);
