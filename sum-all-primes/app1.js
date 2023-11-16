ad28bfb9f58a71a2265e8321e8cde86a48b94b36
AR123456 <coloradoz99@gmail.com>
2023-11-16 05:42:18 -0700

    solved

--- sum-all-primes/app1.js
+++ sum-all-primes/app1.js
@@ -2,17 +2,23 @@
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
+function checkForPrime(i) {
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function sumAllPrimes(num) {
   let total = 0;
   // start with 2 (the first prime number )
   for (let i = 2; i <= num; i++) {
     // check if i is prime and if it is add it to total
-    for (let j = 2; j < i; i++) {
-      if (i % j === 0) {
-        return false;
-      }
+    if (checkForPrime(i)) {
+      total += i;
     }
-    total += 1;
   }
   console.log(total);
 }
