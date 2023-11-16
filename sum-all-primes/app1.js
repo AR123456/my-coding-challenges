b4a5cbc085aa5ce7053d3adb17fbee29e86fe1d7
AR123456 <coloradoz99@gmail.com>
2023-11-16 05:29:47 -0700

    sum just primes

--- sum-all-primes/app1.js
+++ sum-all-primes/app1.js
@@ -2,5 +2,8 @@
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
-function sumAllPrimes() {}
+function sumAllPrimes(num) {
+  let total = 0;
+  console.log(total);
+}
 sumAllPrimes(10);
