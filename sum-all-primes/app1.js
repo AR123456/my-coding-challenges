afabc3eace29fcd152f37d138d02584d9f81974c
AR123456 <coloradoz99@gmail.com>
2023-11-16 05:33:38 -0700

    psudo code

--- sum-all-primes/app1.js
+++ sum-all-primes/app1.js
@@ -4,6 +4,10 @@
 
 function sumAllPrimes(num) {
   let total = 0;
+  // start with 2 (the first prime number )
+  for (let i = 2; i <= num; i++) {
+    // check if i is prime and if it is add it to total
+  }
   console.log(total);
 }
 sumAllPrimes(10);
