02ac57330a0dda599aeb7033d12cdeda823165fa
AR123456 <coloradoz99@gmail.com>
2023-11-18 03:49:15 -0700

    psudo code

---
+++ sort-by-height/app1.js
@@ -0,0 +1,14 @@
+// CHALLENGE 4: SORT BY HEIGHT
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex.
+a = [-1, 150, 190, 170, -1, -1, 160, 180];
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight() {
+  // ignore -1 and keep in place
+  // for the remainder of the values re sort into desending order
+  // truthy is not neg 1
+
+  console.log();
+}
+sortByHeight(a);
