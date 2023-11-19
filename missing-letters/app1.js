8bb03e39cf6abec260290f8cb05671cbc3b07352
AR123456 <coloradoz99@gmail.com>
2023-11-19 06:16:50 -0700

    psudo code

---
+++ missing-letters/app1.js
@@ -0,0 +1,15 @@
+// CHALLENGE 5: MISSING LETTERS
+// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+// ex.
+// missingLetters("abce") == "d"
+// missingLetters("abcdefghjklmno") == "i"
+// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters() {
+  // create array of letters of the alphabet
+  // take in the string of letters , make it an array
+  // loop the array of input and compare to alphabet
+  // if the letter of the input matches the value and position of the input string go on to next letter
+  // if it does not then return the unmatched letter from the alphabet
+  // if everything matches return undefined
+}
