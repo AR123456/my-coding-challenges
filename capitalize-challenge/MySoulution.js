c3fa55272db87284ea9f1e50cf1b7da072637693
AR123456 <coloradoz99@gmail.com>
2023-11-20 05:21:03 -0700

    my solution

---
+++ capitalize-challenge/MySoulution.js
@@ -0,0 +1,8 @@
+// Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
+const myString = "developer";
+
+cap = myString.charAt(0).toUpperCase();
+string2 = myString.slice(1);
+newString = cap + string2;
+
+console.log(newString);
