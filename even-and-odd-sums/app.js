5e713acd484527deb57dc735d6c350f762ba8dde
AR123456 <coloradoz99@gmail.com>
2023-11-21 05:51:37 -0700

    sum adds and evens

---
+++ even-and-odd-sums/app.js
@@ -0,0 +1,16 @@
+// CHALLENGE 6: EVEN & ODD SUMS
+// Take in an array and return an array of the sums of even and odd numbers
+// ex.
+
+const { clearScreenDown } = require("readline");
+
+// evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
+const arr = [50, 60, 60, 45, 71];
+function evenOddSums(arr) {
+  let odds = [];
+  let evens = [];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+  }
+}
+evenOddSums(arr);
