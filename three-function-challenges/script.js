7a70c0e65554c3e5199c66dd2bb1016eacae6a09
AR123456 <coloradoz99@gmail.com>
2023-11-25 05:08:26 -0700

    start of iffe challange

--- three-function-challenges/script.js
+++ three-function-challenges/script.js
@@ -60,3 +60,16 @@ const minMax = () => {
 };
 
 console.log(minMax(arr));
+
+// /////////////////////////////////////////////
+/* 
+## Challenge 3
+
+Create an IIFE (Immediately Invoked Function Expression) that takes in the length and width of a rectangle outputs it to the console in a message as soon as the page loads.
+
+**Expected Result:**
+
+```JavaScript
+// On page load
+The area of a rectangle with a length of 10 and a width of 5 is 50. */
+alert("The area of a rectangle with a length of 10 and a width of 5 is 50");
