43f2e7ef8847e58b0ef3d658eae27c84d7fb1e9c
AR123456 <coloradoz99@gmail.com>
2023-11-28 05:49:54 -0700

    palindrome validation

---
+++ palindrome-validation/app1.js
@@ -0,0 +1,6 @@
+// CHALLENGE 2: VALIDATE A PALINDROME
+//https://www.youtube.com/watch?v=M2bJBuaOeOQ
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) {}
