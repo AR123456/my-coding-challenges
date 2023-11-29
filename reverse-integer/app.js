f221663a9343da2c26e13373213826337d02d329
AR123456 <coloradoz99@gmail.com>
2023-11-29 03:45:27 -0700

    solved with es6

--- reverse-integer/app.js
+++ reverse-integer/app.js
@@ -2,9 +2,16 @@
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
+// let int = 125;
+// function reverseInt(int) {
+//   console.log(parseFloat(int.toString().split("").reverse().join("")));
+// }
+
+// reverseInt(int);
+
 let int = 125;
-function reverseInt(int) {
+const reverseInt = (int) => {
   console.log(parseFloat(int.toString().split("").reverse().join("")));
-}
+};
 
 reverseInt(int);
