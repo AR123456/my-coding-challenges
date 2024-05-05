d652b031ececf0600805cbfb08d46ad24b85faaf
AR123456 <coloradoz99@gmail.com>
2024-05-05 06:49:12 -0400

    seekAndDistroy

---
+++ seek-distroy/sol.js
@@ -0,0 +1,7 @@
+// CHALLENGE 3: SEEK & DESTROY
+// Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
+// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+
+const arr = [2, 3, 4, 6, 6, "hello"];
+let remove = [2, 6];
+function seekAndDestroy() {}
