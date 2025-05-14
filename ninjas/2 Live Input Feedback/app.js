a76e26db909c34e0b0e6feefc1405e3bc68f6168
AR123456 <coloradoz99@gmail.com>
2025-05-14 07:06:47 -0400

    live email input form field

---
+++ ninjas/2 Live Input Feedback/app.js
@@ -0,0 +1,36 @@
+const input = document.querySelector("input");
+const tickMark = document.querySelector(".tick");
+const formBox = document.querySelector("form input");
+//  onkeyup run this function
+input.addEventListener("keyup", (e) => {
+  validateEmail(e.key);
+});
+function validateEmail() {
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // The value property sets or returns the value of the value attribute of a text field.The value property contains the default value OR the value a user types in (or a value set by a script).
+  const isValid = emailRegex.test(input.value);
+
+  if (isValid) {
+    // turn check mark green
+    tickMark.style.visibility = "visible";
+    tickMark.style.color = "green";
+    if (input.value.length > 0) {
+      formBox.style.border = "2px solid green";
+    }
+
+    // border: 2px solid #777;
+    console.log("valid");
+  } else {
+    tickMark.style.color = "red";
+    console.log("Invalid email address");
+  }
+}
+
+// const validateEmail = (email) => {
+//   return String(email)
+//     .toLowerCase()
+//     .match(
+//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     );
+// };
