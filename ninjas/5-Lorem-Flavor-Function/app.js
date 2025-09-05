4f6efee20b8689da395b155c7f3b550c9f655b0c
AR123456 <coloradoz99@gmail.com>
2025-09-05 08:17:11 -0400

    problem statement

---
+++ ninjas/5-Lorem-Flavor-Function/app.js
@@ -0,0 +1,9 @@
+// buttons, textarea and output
+
+// flavor arrays
+
+// function to take inputText and the flavor array generate new text replace every 3rd word with random flavor word
+
+// function to output the text into the output area
+
+// event listener to detect button click on one of the buttons
---
+++ ninjas/5-Lorem-Flavor-Function/problem.md
@@ -0,0 +1,4 @@
+### Take some standard Lorem Ipsum and replace with a lorem theme
+
+Form textarea to paste in the lorem and then div "buttons" with button X 3 , p tag and buttons x3 , give buttons a data-flavor attribute to use in on click
+Below form text output div
