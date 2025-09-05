34e784f106e6cbcaa6f988a53299b11689673c5c
AR123456 <coloradoz99@gmail.com>
2025-09-05 08:17:35 -0400

    html and the js array

--- ninjas/5-Lorem-Flavor-Function/app.js
+++ ninjas/5-Lorem-Flavor-Function/app.js
@@ -1,7 +1,42 @@
 // buttons, textarea and output
 
 // flavor arrays
-
+const summerWords = [
+  "sunshine",
+  "beach",
+  "icecream",
+  "sandals",
+  "picnic",
+  "swim",
+  "campfire",
+  "vacation",
+  "hammock",
+  "lemonade",
+];
+const natureWords = [
+  "forest",
+  "river",
+  "mountain",
+  "meadow",
+  "breeze",
+  "wildflower",
+  "rain",
+  "stone",
+  "tree",
+  "sunrise",
+];
+const musicWords = [
+  "melody",
+  "rhythm",
+  "chorus",
+  "guitar",
+  "piano",
+  "drums",
+  "harmony",
+  "lyrics",
+  "concert",
+  "violin",
+];
 // function to take inputText and the flavor array generate new text replace every 3rd word with random flavor word
 
 // function to output the text into the output area
