8ebec70b117b5393bd5ff62162eed4767988c813
AR123456 <coloradoz99@gmail.com>
2025-09-08 07:43:02 -0400

    back to prevent default

--- ninjas/5-Lorem-Flavor-Function/app.js
+++ ninjas/5-Lorem-Flavor-Function/app.js
@@ -48,25 +48,16 @@ const musicWords = [
 //////////////////////// go dev example //////////////
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/paste_event
 inputText.addEventListener("paste", (event) => {
-  let pastedText = (event.clipboardData || window.clipboardData).getData(
-    "text"
-  );
-  handlePaste(pastedText);
+  pastedText = (event.clipboardData || window.clipboardData).getData("text");
 });
 
 // function to take inputText and the flavor array generate new text replace every 3rd word with random flavor word
-function handlePaste(text) {
-  summerButton.addEventListener("click", (event) => {
-    event.preventDefault();
 
-    console.log("summer", text);
-  });
-}
 // function to output the text into the output area
 // buttons event listeners
 summerButton.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log("summer");
+  console.log(summerWords, pastedText);
 });
 natureButton.addEventListener("click", (event) => {
   event.preventDefault();
