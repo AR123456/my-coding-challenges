29dbee08104f97fe1332809e53cee0cda757d7ac
AR123456 <coloradoz99@gmail.com>
2025-09-18 07:00:22 -0400

    adding placeholder text

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -1,10 +1,10 @@
 const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
-console.log(password);
-password.addEventListener("focus", () => {
-  console.log(password);
-});
-password.addEventListener("blur", () => {
-  console.log("out");
-});
+let passwordValue = password.value;
+// password.addEventListener("focus", () => {
+//   console.log(passwordValue);
+// });
+// password.addEventListener("blur", () => {
+//   console.log("out");
+// });
