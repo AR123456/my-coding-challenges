7220a777df4bc7905c6c445a91514229bce70a00
AR123456 <coloradoz99@gmail.com>
2025-09-18 06:59:26 -0400

    add event listener

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -2,4 +2,6 @@ const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
 
-console.log(password);
+password.addEventListener("focus", () => {
+  console.log(value);
+});
