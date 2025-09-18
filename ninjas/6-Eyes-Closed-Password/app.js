59f62b9625ba1035e9284219209e55c81209ac38
AR123456 <coloradoz99@gmail.com>
2025-09-18 06:59:55 -0400

    trying to detect value

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -1,7 +1,10 @@
 const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
-
+console.log(password);
 password.addEventListener("focus", () => {
-  console.log(value);
+  console.log(password);
+});
+password.addEventListener("blur", () => {
+  console.log("out");
 });
