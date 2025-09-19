6e42a75c77194c9c7f6e1d483af98dfd4f9be1c1
AR123456 <coloradoz99@gmail.com>
2025-09-19 08:22:52 -0400

    password input event

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -1,10 +1,12 @@
 const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
-let passwordValue = password.value;
-// password.addEventListener("focus", () => {
-//   console.log(passwordValue);
-// });
-// password.addEventListener("blur", () => {
-//   console.log("out");
-// });
+
+password.detectInput = addEventListener("input", (event) => {
+  console.log(password.value);
+});
+
+password.addEventListener("focus", () => {});
+password.addEventListener("blur", () => {
+  console.log("out");
+});
