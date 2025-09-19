0b5f46666019a615dc3c896407db471d7862aa5f
AR123456 <coloradoz99@gmail.com>
2025-09-19 08:24:29 -0400

    trying to toggle

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -1,7 +1,11 @@
 const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
+const hide = document.querySelector(".hide");
 
+const toggleEyes = () => {
+  eyes.style.display === "none";
+};
 password.addEventListener("focus", () => {
   password.detectInput = addEventListener("input", (event) => {
     console.log("close your eyes");
@@ -9,4 +13,5 @@ password.addEventListener("focus", () => {
 });
 password.addEventListener("blur", () => {
   console.log("open you eyes");
+  toggleEyes();
 });
