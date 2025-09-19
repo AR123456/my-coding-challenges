6ae91352aa1b56cb11419a40dbdd79a43989d0c5
AR123456 <coloradoz99@gmail.com>
2025-09-19 08:23:27 -0400

    in focus in pw field

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -2,11 +2,11 @@ const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
 
-password.detectInput = addEventListener("input", (event) => {
-  console.log(password.value);
+password.addEventListener("focus", () => {
+  password.detectInput = addEventListener("input", (event) => {
+    console.log(password.value);
+  });
 });
-
-password.addEventListener("focus", () => {});
 password.addEventListener("blur", () => {
   console.log("out");
 });
