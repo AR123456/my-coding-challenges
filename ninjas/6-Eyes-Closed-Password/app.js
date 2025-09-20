a191300eb8b054448b24d493cccc3f247ded2de6
AR123456 <coloradoz99@gmail.com>
2025-09-20 06:33:53 -0400

    making new set eyes closed function

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -3,11 +3,19 @@ const password = document.querySelector("#password");
 const openImg = document.querySelector(".eyes.open img");
 const closedImg = document.querySelector(".eyes.closed img");
 
-const setEyesClosed = (closed) => {};
+const setEyesClosed = (closed) => {
+  if (closed) {
+    openImg.style.display = "none";
+    closedImg.style.display = "inline";
+  } else {
+    openImg.style.display = "inline";
+    closedImg.style.display = "none";
+  }
+};
 password.addEventListener("focus", () => {
   password.detectInput = addEventListener("input", (event) => {
     console.log("close your eyes");
-    toggleEyes();
+    setEyesClosed();
   });
 });
 password.addEventListener("blur", () => {
