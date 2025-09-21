822bfe7d5740bfc24d1347df59ca431cbb186ce0
AR123456 <coloradoz99@gmail.com>
2025-09-21 08:32:59 -0400

    style in line

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -1,33 +1,13 @@
+const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
-const openImg = document.querySelector(".eyes.open img");
-const closedImg = document.querySelector(".eyes.closed img");
-
-const setEyesClosed = (closed) => {
-  if (closed) {
-    openImg.style.display = "none";
-    closedImg.style.display = "inline";
-  } else {
-    openImg.style.display = "inline";
-    closedImg.style.display = "none";
-  }
-};
+const passClass = document.querySelector(".password");
 password.addEventListener("focus", () => {
-  // password.detectInput = addEventListener("input", (event) => { console.log("close your eyes"); toggleEyes();
-
-  // });
   password.detectInput = addEventListener("input", (event) => {
-    console.log("input -> keep them closed");
-    setEyesClosed(true);
+    console.log("close your eyes");
   });
 });
-
-password.detectInput = addEventListener("input", (event) => {
-  console.log("input -> keep them closed");
-  setEyesClosed(true);
-});
-
 password.addEventListener("blur", () => {
-  console.log("blur -> open your eyes");
-  setEyesClosed(false);
+  console.log("open you eyes");
+  passClass.toggleAttribute();
 });
