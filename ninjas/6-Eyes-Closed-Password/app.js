7ffe14ddffa55c8e44ea2355f43e9fb3775422a6
AR123456 <coloradoz99@gmail.com>
2025-09-21 08:33:32 -0400

    targeting img div

--- ninjas/6-Eyes-Closed-Password/app.js
+++ ninjas/6-Eyes-Closed-Password/app.js
@@ -1,13 +1,23 @@
 const eyes = document.querySelector(".eyes");
 const userName = document.querySelector("#user-name");
 const password = document.querySelector("#password");
-const passClass = document.querySelector(".password");
+const openImg = document.querySelector(".eyes.open img");
+const closedImg = document.querySelector(".eyes.closed img");
+
+const toggleEyes = () => {
+  let closed = document.querySelector(".closed");
+  if (closed.style.display === "inline") {
+    closed.style.display = "none";
+  } else {
+    closed.style.display = "inline";
+  }
+};
 password.addEventListener("focus", () => {
   password.detectInput = addEventListener("input", (event) => {
     console.log("close your eyes");
+    toggleEyes();
   });
 });
 password.addEventListener("blur", () => {
   console.log("open you eyes");
-  passClass.toggleAttribute();
 });
