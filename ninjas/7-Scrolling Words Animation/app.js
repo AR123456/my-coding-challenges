7534b11f6da15eb9ecdf0b13ca3087c12ffac6ba
AR123456 <coloradoz99@gmail.com>
2025-09-26 10:02:53 -0400

    not working

--- ninjas/7-Scrolling Words Animation/app.js
+++ ninjas/7-Scrolling Words Animation/app.js
@@ -1,3 +1,8 @@
 const list = document.querySelector(".word-list");
 const items = list.querySelectorAll("span");
 let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % items.length;
+  list.style.transform = `translateY(=${index * 30}px)`;
+}, 2000);
