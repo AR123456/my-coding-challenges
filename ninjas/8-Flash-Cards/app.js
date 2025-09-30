010696e2d2831c77e0534d725a34792278ac76a2
AR123456 <coloradoz99@gmail.com>
2025-09-30 06:06:31 -0400

    add transform

--- ninjas/8-Flash-Cards/app.js
+++ ninjas/8-Flash-Cards/app.js
@@ -1,8 +1,10 @@
 const question = document.querySelector(".question");
 const answer = document.querySelector(".answer");
+const card = document.querySelector(".card");
 
 question.addEventListener("click", () => {
   console.log("click");
+  card.style.transform = "rotateY(180deg)";
   answer.classList.remove("hidden");
   question.classList.add("hidden");
 });
