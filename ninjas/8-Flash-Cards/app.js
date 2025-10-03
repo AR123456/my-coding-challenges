53f4ec7418425deef25e975f2167a1dfc255a96c
AR123456 <coloradoz99@gmail.com>
2025-10-03 08:04:47 -0400

    re do

---
+++ ninjas/8-Flash-Cards/app.js
@@ -0,0 +1,12 @@
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
+const card = document.querySelector(".card");
+
+question.addEventListener("click", () => {
+  console.log("click");
+  card.style.transform = "rotateY(180deg)";
+  answer.classList.remove("hidden");
+  question.classList.add("hidden");
+});
+// https://css-tricks.com/controlling-css-animations-transitions-javascript/
+const turn = () => {};
